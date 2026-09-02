ded33b51fe599c7692cad87b64cc83385b808b77
emmanuelfaguir <emmanuelgatluak@gmail.com>
2026-09-02 14:40:53 +0200

    Fix formatting of URL assignment in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 app.use(express.json());
 app.use(express.static('public'));
-const URL=(process.env.SUPABASE_URL||'').replace(/\\/$/,'');
+const URL = (process.env.SUPABASE_URL || '').replace(/\/$/, '');
 const KEY=process.env.SUPABASE_KEY||'';
 async function supa(path,options={}){
   const r=await fetch(URL+path,{...options,headers:{
